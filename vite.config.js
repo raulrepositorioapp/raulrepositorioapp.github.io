b0ecea91d621400a2c65fb20eee2a7a04c52ba00
SomorMK <samormk6@gmail.com>
2026-01-16 11:37:07 +0600

    github pages modifications addade

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/raulrepositorioapp.github.io/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
